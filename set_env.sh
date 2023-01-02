b18447e533930c5f6ead347204a62255ee0602dd
Mostafa Attia <MostafaMohammedAttia@gmail.com>
2023-01-02 22:50:57 +0200

    refactor from monolithic to microservice

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=mostafaattia
+export POSTGRES_PASSWORD=MostafaAttia123
+export POSTGRES_HOST=mostafaattia-project-4-db.cttoeggs9igy.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=mostafaattia-prject-4
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
